7907563e298f4a2aa21eef925395b86070ffd487
Aaron Tidwell <atidwell@businessinsider.com>
2012-04-19 23:47:10 -0400

    added location cmsPath helper

---
+++ assets/js/lib/BI/location/cmsPath.js
@@ -0,0 +1,53 @@
+/**
+	Used to store and retrieve data about the server path to the library
+	(for including scripts, css, relative links, etc)
+
+	@module cmsPath
+**/
+
+/**
+	@class cmsPath
+	@namespace BI.location
+**/
+(function($,ko,BI,undefined) {
+	var exposed;
+
+	var paths = {};
+
+	BI.namespace('location')
+
+	/** 
+		Sets a path
+		
+		@method set
+		@param {String} key
+		@param {String} value
+
+		@return {void}
+	**/
+
+	function set(key,value) {
+	  paths[key] = value;
+	}
+
+	/** 
+		Gets a path
+		
+		@method get
+		@param {String} key
+		@param {String} value
+
+		@return {void}
+	**/
+
+	function get(key) {
+	  return paths[key];
+	}
+
+	exposed = {
+		set: set,
+		get: get
+	}
+
+	BI.location.cmsPath = exposed;
+}(jQuery, ko, BI))
--- docs/api.js
+++ docs/api.js
@@ -2,28 +2,24 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "BI",
+        "BI.location.cmsPath",
         "BI.location.querystring",
         "BI.mvvm"
     ],
     "modules": [
-        "base",
-        "mvvm",
-        "querystring"
+        "cmsPath",
+        "mvvm"
     ],
     "allModules": [
         {
-            "displayName": "base",
-            "name": "base",
-            "description": "Define our global BI variable (and organization methods)\nThis is a singleton defined using the Revealing Module Pattern"
+            "displayName": "cmsPath",
+            "name": "cmsPath",
+            "description": "Used to store and retrieve data about the server path to the library\n(for including scripts, css, relative links, etc)"
         },
         {
             "displayName": "mvvm",
             "name": "mvvm",
             "description": "Custom helper functions for doing basic tasks with data sets and knockout\nAbstracted as MVVM should we ever need to replace knockout"
-        },
-        {
-            "displayName": "querystring",
-            "name": "querystring"
         }
     ]
 } };
